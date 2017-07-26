2fc5822a246414a95898aa4d9bb190be45d0c289
Neutrinoq <keilvictor60@gmail.com>
2017-07-26 03:09:29 +0300

    turn string literals into variables

--- tempignore.py
+++ tempignore.py
@@ -5,6 +5,10 @@ writefile = None
 isignored = False;
 isfileexists = True
 
+inactive_style_names = ["../inactive_styles/light_blue.css ", "../inactive_styles/dark_blue.css ", "../inactive_styles/dark_blue_orange.css "]
+file_path = "../css/style.css"
+
+
 try:
     readfile = open("ignore_state.txt")
 except FileNotFoundError:
@@ -17,11 +21,11 @@ if isfileexists:
     elif line == "1":
         isignored = True
 
-inactive_style_names = ["../inactive_styles/light_blue.css ", "../inactive_styles/dark_blue.css ", "../inactive_styles/dark_blue_orange.css "]
+
 writefile = open("ignore_state.txt", "w")
 if isignored:
     writefile.write("0")
-    subprocess.call('git update-index --no-assume-unchanged ../css/style.css ' + inactive_style_names[0] + inactive_style_names[1] + inactive_style_names[2], shell = True)
+    subprocess.call('git update-index --no-assume-unchanged ' + file_path + inactive_style_names[0] + inactive_style_names[1] + inactive_style_names[2], shell = True)
 else:
     writefile.write("1")
-    subprocess.call('git update-index --assume-unchanged ../css/style.css ' + inactive_style_names[0] + inactive_style_names[1] + inactive_style_names[2], shell = True)
+    subprocess.call('git update-index --assume-unchanged ' + file_path + inactive_style_names[0] + inactive_style_names[1] + inactive_style_names[2], shell = True)
